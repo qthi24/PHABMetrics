c67184f069eecd69033b3ea56c4d94ebfcbe6d34
fawda123 <mbafs2012@gmail.com>
2018-11-02 16:48:07 -0700

    bankstability metrics work with sampdat

---
+++ R/bankstability.R
@@ -0,0 +1,41 @@
+#' Bank stability metrics
+#'
+#' @param data Input data
+#' 
+#' @export
+#' 
+#' @examples 
+#' bankstability(sampdat)
+bankstability <- function(data){
+  data <- data[which(data$AnalyteName %in% c('Bank Stability')),]
+  ###PBM_S###
+  
+  stable <- function(data){
+    length(which(data == "stable"))
+  }
+  total <- function(data){length(c(which(data == "stable"), 
+                                   which(data == "vulnerable"), which(data == "eroded")))}
+  PBM_S_sum <- tapply(data$VariableResult, data$id, stable)
+  total_obs <- tapply(data$VariableResult, data$id, total)
+  PBM_S.result <- (PBM_S_sum/total_obs)*100
+  
+  ###PBM_V###
+  vulnerable <- function(data){
+    length(which(data == "vulnerable"))
+  }
+  PBM_V_sum <- tapply(data$VariableResult, data$id, vulnerable)
+  PBM_V.result <- (PBM_V_sum/total_obs)*100
+  
+  ###PBM_E###
+  eroded <- function(data){
+    length(which(data == "eroded"))
+  }
+  PBM_E_sum <- tapply(data$VariableResult, data$id, eroded)
+  PBM_E.result <- (PBM_E_sum/total_obs)*100
+  
+  ###Write to file###
+  results <- cbind(PBM_S.result, PBM_V.result, PBM_E.result)
+
+  return(results)
+  
+}
