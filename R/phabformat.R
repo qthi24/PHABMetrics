1a97c53384e234e2ecc36414ec7da576d96145c0
r7butler <40872002+r7butler@users.noreply.github.com>
2018-11-26 12:32:53 -0800

    Update phabformat.R

--- R/phabformat.R
+++ R/phabformat.R
@@ -9,12 +9,12 @@
 #' @examples
 #' phabformat(sampdat)
 phabformat <- function(data){
-  data <- data.frame(data)
+  data <- tibble(data)
   data$VariableResult[data$ResQualCode=="NR"] <- "Not Recorded"
   data$Result[data$ResQualCode=="NR"] <- NA
   data <- data %>% 
     tidyr::unite('id', StationCode, SampleDate, Replicate, remove = F) %>% 
     data.frame(stringsAsFactors = F)
-  return(data)
+  return(tibble(data))
 }
 
