b77e03787194243b03d954a5ae0a438b28c941ae
r7butler <40872002+r7butler@users.noreply.github.com>
2018-11-27 12:33:47 -0800

    Update phabformat.R

--- R/phabformat.R
+++ R/phabformat.R
@@ -9,9 +9,8 @@
 #' @examples
 #' phabformat(sampdat)
 library(tibble)
-
 phabformat <- function(data){
-  data <- as_tibble(data)
+  data <- as.tibble(data)
   data$VariableResult[data$ResQualCode=="NR"] <- "Not Recorded"
   data$Result[data$ResQualCode=="NR"] <- NA
   data <- data %>% 
