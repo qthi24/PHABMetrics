05d12f7678482b72e3b37a2dcf35869b704ebf75
r7butler <40872002+r7butler@users.noreply.github.com>
2019-07-02 14:54:09 -0700

    Debugging.

--- R/bankmorph.R
+++ R/bankmorph.R
@@ -73,8 +73,8 @@ bankmorph <- function(data){
   # The Below line of code which is commented out counts the number of non null observations
   # This is correct per the instructions. However, it appears that the legacy calculator is including nulls
   # I will change the code to count null observations and then see if the numbers match
-  # XWIDTH.count <- tapply(XWIDTHdata$result, XWIDTHdata$id, lengthna)
-  XWIDTH.count <- tapply(XWIDTHdata$result, XWIDTHdata$id, length) # This is wrong, but necessary to match legacy.
+  XWIDTH.count <- tapply(XWIDTHdata$result, XWIDTHdata$id, lengthna)
+  #XWIDTH.count <- tapply(XWIDTHdata$result, XWIDTHdata$id, length) # This is wrong, but necessary to match legacy.
   XWIDTH.result <- round(XWIDTH_sum/XWIDTH.count, 1)
   
   print("XWIDTHdata$result")
