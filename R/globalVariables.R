bd1f86765653d3b26ae0b8c6079a531a38f729ea
fawda123 <mbafs2012@gmail.com>
2019-07-05 14:04:56 -0700

    version 2.0 bump, added RB as author

--- R/globalVariables.R
+++ R/globalVariables.R
@@ -1,7 +1,9 @@
 globalVariables(c('XMIAT_SDSs', 'XMIATP_SDSs', '.', 'AnalyteName', 'LocationCode', 
                   'LocationCode2', 'Replicate', 'Result', 'SampleDate',
                   'StationCode', 'VariableResult', 'dup', 'id', 'ind', 'n', 'pull',
-                  'trans', 'view', 'UnitName', 'val', 'FractionName', 'QACode', 'ResQualCode'
+                  'trans', 'view', 'UnitName', 'val', 'FractionName', 'QACode', 'ResQualCode',
+                  'H_AqHab.count', 'H_AqHab.result', 'H_FlowHab.count', 'H_FlowHab.result', 
+                  'H_SubNat.count', 'H_SubNat.result', 'convert'
 ))
 
 #' @importFrom stats quantile sd
